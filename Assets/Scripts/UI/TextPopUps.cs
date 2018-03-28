d4a5406f79d3a4537d092dca1a655ab9b6ffc504
oops-accidental <smp5119@gmail.com>
2018-03-27 19:24:28 -0700

    work on text pop ups

---
+++ Assets/Scripts/UI/TextPopUps.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+public class TextPopUps : MonoBehaviour {
+
+    public static TextPopUps instance;
+    public Sprite strike, outImg, homeRun;
+    public GameObject popUpObj;
+    public Image popUpImg;
+
+	void Start () {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
+        popUpObj.SetActive(false);
+        popUpImg = popUpObj.GetComponent<Image>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ShowPopUp(string type)
+    {
+        switch (type)
+        {
+            case "strike":
+                popUpImg.sprite = strike;
+                break;
+            case "out":
+                popUpImg.sprite = outImg;
+                break;
+            case "homerun":
+                popUpImg.sprite = homeRun;
+                break;
+        }
+    }
+}
